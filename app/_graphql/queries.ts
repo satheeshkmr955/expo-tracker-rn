d697c11efd22811047cc07f614d9322142eb08fc
Satheeshkumar P <satheesh@trendlyne.com>
2024-05-16 23:34:20 +0530

    added queries for tracks

--- app/_graphql/queries.ts
+++ app/_graphql/queries.ts
@@ -1,3 +1,5 @@
+"use client";
+
 import { graphql } from "@/gql";
 
 export const UserDetails = graphql(/* GraphQL */ `
@@ -6,6 +8,40 @@ export const UserDetails = graphql(/* GraphQL */ `
     name
     email
     slugName
+  }
+`);
+
+export const TrackDetails = graphql(/* GraphQL */ `
+  fragment TrackDetails on Track {
+    id
+    name
+    userId
+    user {
+      ...UserDetails
+    }
+  }
+`);
+
+export const PointDetails = graphql(/* GraphQL */ `
+  fragment PointDetails on Point {
+    id
+    timestamp
+
+    latitude
+    longitude
+    altitude
+    accuracy
+    heading
+    speed
+  }
+`);
+
+export const TrackWithPoint = graphql(/* GraphQL */ `
+  fragment TrackWithPoint on Track {
+    ...TrackDetails
+    locations {
+      ...PointDetails
+    }
   }
 `);
 
@@ -29,7 +65,6 @@ export const GetSelfByName = graphql(/* GraphQL */ `
   query GetSelfByName($input: GetSelfByNameInput!) {
     getSelfByName(input: $input) {
       ...UserDetails
-      bio
     }
   }
 `);
@@ -38,6 +73,27 @@ export const GetUserByName = graphql(/* GraphQL */ `
   query GetUserByName($input: GetUserByNameInput!) {
     getUserByName(input: $input) {
       ...UserDetails
+    }
+  }
+`);
+
+export const GetTracks = graphql(/* GraphQL */ `
+  query GetTracks($input: GetTracksInput!) {
+    getTracks(input: $input) {
+      items {
+        ...TrackWithPoint
+      }
+      pagination {
+        ...PaginationDetails
+      }
+    }
+  }
+`);
+
+export const CreateTrack = graphql(/* GraphQL */ `
+  mutation CreateTrack($input: CreateTrackInput!) {
+    createTrack(input: $input) {
+      ...TrackWithPoint
     }
   }
 `);
